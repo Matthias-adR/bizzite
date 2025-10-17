04980667a52c1b00f0b2c39e39ccd14e2a068a52
Matthias an der Ruhr <135343836+Matthias-adR@users.noreply.github.com>
2025-10-17 21:53:05 +0200

    initial packages

--- build_files/build.sh
+++ build_files/build.sh
@@ -2,6 +2,9 @@
 
 set -ouex pipefail
 
+systemctl enable systemd-timesyncd
+systemctl enable systemd-resolved.service
+
 ### Install packages
 
 # Packages can be installed from any enabled yum repo on the image.
@@ -10,7 +13,8 @@ set -ouex pipefail
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
 # this installs a package from fedora repos
-dnf5 install -y tmux 
+dnf -y install \
+    uxplay     \
 
 # Use a COPR Example:
 #
@@ -18,7 +22,29 @@ dnf5 install -y tmux
 # dnf5 -y install package
 # Disable COPRs so they don't end up enabled on the final image:
 # dnf5 -y copr disable ublue-os/staging
+dnf -y copr enable yalter/niri
+dnf -y copr disable yalter/niri
+dnf -y --enablerepo copr:copr.fedorainfracloud.org:yalter:niri install niri
+rm -rf /usr/share/doc/niri
+
+dnf -y install \
+    brightnessctl \
+    gnome-keyring \
+    nautilus \
+    tuigreet \
+    udiskie \
+    wlsunset \
+
+add_wants_niri() {
+    sed -i "s/\[Unit\]/\[Unit\]\nWants=$1/" "/usr/lib/systemd/user/niri.service"
+}
+add_wants_niri noctalia.service
+add_wants_niri plasma-polkit-agent.service
+add_wants_niri swayidle.service
+add_wants_niri udiskie.service
+add_wants_niri xwayland-satellite.service
+cat /usr/lib/systemd/user/niri.service
 
 #### Example for enabling a System Unit File
 
-systemctl enable podman.socket
+#systemctl enable podman.socket
